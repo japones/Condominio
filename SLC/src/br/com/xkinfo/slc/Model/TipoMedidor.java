ef7d68a6416e26d553004181a03ec01c9df38b8e
japones <alexandre.homma@gmail.com>
2014-07-15 13:32:17 -0300

    Correcoes

---
+++ SLC/src/br/com/xkinfo/slc/Model/TipoMedidor.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.xkinfo.slc.Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Alexandre
+ */
+public class TipoMedidor implements java.io.Serializable {
+    
+    private Integer id;
+     private String descricao;
+     private Usuario usuarioinclusao;
+     private Date datainclusao;
+     private Usuario usuarioalteracao;
+     private Date dataalteracao;
+
+    public TipoMedidor() {
+    }
+	
+    public TipoMedidor(String descricao, Usuario usuarioinclusao, Date datainclusao) {
+        this.descricao = descricao;
+        this.usuarioinclusao = usuarioinclusao;
+        this.datainclusao = datainclusao;
+    }
+    
+    
+    public TipoMedidor(String descricao, Usuario usuarioinclusao, Date datainclusao, Usuario usuarioalteracao, Date dataalteracao) {
+       this.descricao = descricao;
+       this.usuarioinclusao = usuarioinclusao;
+       this.datainclusao = datainclusao;
+       this.usuarioalteracao = usuarioalteracao;
+       this.dataalteracao = dataalteracao;
+    }
+   
+    public Integer getId() {
+        return this.id;
+    }
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public String getDescricao() {
+        return this.descricao;
+    }
+    
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    public Usuario getUsuarioinclusao() {
+        return this.usuarioinclusao;
+    }
+    
+    public void setUsuarioinclusao(Usuario usuarioinclusao) {
+        this.usuarioinclusao = usuarioinclusao;
+    }
+    
+    public Date getDatainclusao() {
+        return this.datainclusao;
+    }
+    
+    public void setDatainclusao(Date datainclusao) {
+        this.datainclusao = datainclusao;
+    }
+    
+    public Usuario getUsuarioalteracao() {
+        return this.usuarioalteracao;
+    }
+    
+    public void setUsuarioalteracao(Usuario usuarioalteracao) {
+        this.usuarioalteracao = usuarioalteracao;
+    }
+    
+    public Date getDataalteracao() {
+        return this.dataalteracao;
+    }
+    
+    public void setDataalteracao(Date dataalteracao) {
+        this.dataalteracao = dataalteracao;
+    }
+
+}
